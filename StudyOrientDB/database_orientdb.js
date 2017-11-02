b101d738e8425917d2bb6d0a7f61ebc3a8ec9e99
geuni <geuni0110@m-teletech.com>
2017-11-02 13:37:37 +0900

    db plan

--- StudyOrientDB/database_orientdb.js
+++ StudyOrientDB/database_orientdb.js
@@ -23,12 +23,40 @@ db.record.get('#26:0')
 //    console.log(results);
 // });
 
-var sql = 'SELECT FROM topic WHERE @rid=:rid';
-var param = {
+// var sql = 'SELECT FROM topic WHERE @rid=:rid';
+// var param = {
+//     params: {
+//         rid:'#27:0'
+//     }
+// };
+// db.query(sql, param).then(function(results){
+//     console.log(results);
+// });
+
+// INSERT
+// var sql = "INSERT INTO topic (title, description) VALUES(:title, :desc)";
+//
+// db.query(sql, {
+//     params:{
+//         title: 'Geuni',
+//         desc: 'Geuni is Best'
+//     }
+// }).then(function(results){
+//     console.log(results);
+// });
+
+// UPDATE
+// var sql = "UPDATE topic SET title=:title WHERE @rid=:rid";
+// db.query(sql, {params:{title:'GeuniUpdate', rid:'#26:1'}}).then(function(results) {
+//     console.log(results);
+// });
+
+// DELETE
+var sql = "DELETE FROM topic WHERE @rid=:rid";
+db.query(sql, {
     params: {
-        rid:'#27:0'
+        rid:'#26:1'
     }
-};
-db.query(sql, param).then(function(results){
-    console.log(results);
+}).then(function (result) {
+   console.log(result);
 });
